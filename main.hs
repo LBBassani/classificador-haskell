22925b4835a6ddd0cdb8841563ddf4d0b295fc12
LBBassani <lorenabassani12@gmail.com>
2020-11-04 10:57:37 -0300

    Desenvolvimento da main Desenvolvida a main do primeiro trabalho computacional, já realizando todas as etapas do trabalho.

--- main.hs
+++ main.hs
@@ -6,8 +6,14 @@ import ClassifierAnalysis
 import Text.Printf
 
 {-
+    Primeiro trabalho computacional de Programação Funcional em Haskell. Realiza o treinamento de dois classificadores
+        (Centroide e Vizinho mais próximo) e analisa as acurácias e matriz de confusão da aplicação destes nas bases de
+        teste.
+-}
+
 main :: IO ()
-main = do   putStr("Forneca o nome do arquivo de entrada: ")
+main = do   --leitura dos parametros de entrada
+            putStr("Forneca o nome do arquivo de entrada: ")
             entrada <- getLine
             putStr("Forneca o nome do arquivo de saida: ")
             saida <- getLine
@@ -15,16 +21,25 @@ main = do   putStr("Forneca o nome do arquivo de entrada: ")
             percent <- le_num
             putStr("Forneca o valor da semente para geracao randomizada: ")
             semente <- le_num
-            putStrLn(entrada ++ " " ++ saida ++ " " ++ show percent ++ " " ++ show semente)
--}
-
-main :: IO ()
-main = do   dados <- readFile "iris.csv"
-            let base_treino = fst $ base_treino_teste dados 30 42
-            let base_teste = snd $ base_treino_teste dados 30 42
+            -- Preparo das bases de treino e teste
+            dados <- readFile entrada
+            let base = registros dados
+            let base_treino = fst $ base_treino_teste base percent semente
+            let base_teste = snd $ base_treino_teste base percent semente
+            let classes = descobre_classes base
+            -- Resultados do classificador Nearest Neighbor
             let resultados = roda_classificador_nn base_treino base_teste
-            putStr("Acuracia do NN: " )
-            printf "%.2f\n" (analisa_acuracia $ snd resultados)
+            putStr("Acuracia(vizinho): " )
+            printf "%.2f" (analisa_acuracia $ snd resultados)
+            putStrLn("%")
+            let matriz_confusao = monta_matriz_confusao classes $ snd resultados
+            let escrever_saida_vizinho = "vizinho mais próximo:\n" ++ matriz_to_string matriz_confusao ++ "\n"
+            -- Resultados do classificador Centroide
             let resultados = roda_classificador_centroide base_treino base_teste
-            putStr("Acuracia do Centroide: " )
-            printf "%.2f\n" (analisa_acuracia $ snd resultados)
+            putStr("Acuracia(centroide): " )
+            printf "%.2f" (analisa_acuracia $ snd resultados)
+            putStrLn("%")
+            let matriz_confusao = monta_matriz_confusao classes $ snd resultados
+            let escrever_saida_centroide = "centroides:\n" ++ matriz_to_string matriz_confusao ++ "\n"
+            -- Escrevendo no arquivo de saída
+            writeFile saida $ escrever_saida_vizinho ++ escrever_saida_centroide
