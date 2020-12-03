cee87076738f2bf41fc1b865ef2e83f3da948777
LBBassani <lorenabassani12@gmail.com>
2020-12-03 10:46:23 -0300

    Correção do calculo da matriz de confusão unificada

--- ClassifierAnalysis.hs
+++ ClassifierAnalysis.hs
@@ -1,4 +1,4 @@
-module ClassifierAnalysis (analisa_acuracia, monta_matriz_confusao, matriz_confusao_media) where
+module ClassifierAnalysis (analisa_acuracia, monta_matriz_confusao, matriz_confusao_unificada) where
 import Utils
 import DataHandler(Registro)
 
@@ -27,11 +27,14 @@ linha_matriz_confusao classe classes resultados = [ celula_matriz_confusao x pre
 celula_matriz_confusao :: [Char] -> [(Registro, [Char])] -> Int
 celula_matriz_confusao classe preditos = length $ filter (\((_,_),y) -> y == classe) preditos
 
-matriz_confusao_media :: [[[Int]]] -> [[Int]]
-matriz_confusao_media matrizes_confusao = [linha_matriz_confusao_media i matrizes_confusao | i <- [0.. length (matrizes_confusao!!0) - 1 ]]
+-- Monta da matriz de confusão unificada da utilização do k_folds_cross_validation
+matriz_confusao_unificada :: [[[Int]]] -> [[Int]]
+matriz_confusao_unificada matrizes_confusao = [linha_matriz_confusao_unificada i matrizes_confusao | i <- [0.. length (matrizes_confusao!!0) - 1 ]]
 
-linha_matriz_confusao_media :: Int -> [[[Int]]] -> [Int]
-linha_matriz_confusao_media i matrizes_confusao = [ celula_matriz_confusao_media i j matrizes_confusao | j <- [0..length ((matrizes_confusao!!0)!!0) - 1] ]
+-- Monta uma linha da matriz de confusão unificada da utilização do k_folds_cross_validation
+linha_matriz_confusao_unificada :: Int -> [[[Int]]] -> [Int]
+linha_matriz_confusao_unificada i matrizes_confusao = [ celula_matriz_confusao_unificada i j matrizes_confusao | j <- [0..length ((matrizes_confusao!!0)!!0) - 1] ]
 
-celula_matriz_confusao_media :: Int -> Int -> [[[Int]]] -> Int
-celula_matriz_confusao_media i j matrizes_confusao = div (foldr1 (+) [ (x!!i)!!j | x <- matrizes_confusao])  (fromIntegral $ length matrizes_confusao)
+-- Calcula uma celula da matriz de confusão unificada da utilização do k_folds_cross_validation
+celula_matriz_confusao_unificada :: Int -> Int -> [[[Int]]] -> Int
+celula_matriz_confusao_unificada i j matrizes_confusao = foldr1 (+) [ (x!!i)!!j | x <- matrizes_confusao]
